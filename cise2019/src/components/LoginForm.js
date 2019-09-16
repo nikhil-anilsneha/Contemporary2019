1b8f9e465ad0b564b3f50872d9f91d5b29e75300
Zaw <zawlintun1511@gmail.com>
2019-09-16 13:43:16 +1200

    Login Form Updated

--- cise2019/src/components/LoginForm.js
+++ cise2019/src/components/LoginForm.js
@@ -1,49 +1,68 @@
-import React from './node_modules/react';
+import React, { Component } from 'react';
+import './LoginForm.css';
 
-class LoginForm extends React.Component {
-    state = {
-        date: {
-            username: '',
-            password: ''
-        },
-        loading: false,
-        errors: {}
-    };
-
-    onChange = e => this.setState({
-        data: {...this.state.data, [e.target.name]: e.target.value}
-    });
-
-    render() {
-        const { data } = this.state;
-        return (
-            <Form>
-                <Form.Field>
-                   <label htmlFor='Uname'>Name</label> 
-                   <input 
-                   type='Uname' 
-                   id= 'Uname' 
-                   name= 'Uname'
-                   placeholder='example@example.com'
-                   value={data.password}
-                   onChange={this.onChange}
-                   />
-                </Form.Field>
-                <Form.Field>
-                   <label htmlFor='password'>Name</label> 
-                   <input 
-                   type='password' 
-                   id= 'password' 
-                   name= 'password'
-                   placeholder='Make it secure'
-                   value={data.password}
-                   onChange={this.onChange}
-                   />
-                </Form.Field>
-                <Button primary>Login</Button>
-            </Form>
-        );
-    }
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <div className='Form'>
+          <h1>Create Account</h1>
+          <form onSubmit ={this.handleSubmit} noValidate>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+              <input 
+              type ="text" 
+              className='' 
+              placeholder="First Name"
+              type="text"
+              name="firstName"
+              noValidate
+              onChange={this.handleChange}/>
+            </div>
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input 
+              type ="text" 
+              className='' 
+              placeholder="Last Name"
+              type="text"
+              name="lastName"
+              noValidate
+              onChange={this.handleChange}/>
+            </div>
+            <div className="email">
+              <label htmlFor="firstName">Email</label>
+              <input 
+              type ="text" 
+              className='' 
+              placeholder="Email"
+              type="text"
+              name="email"
+              noValidate
+              onChange={this.handleChange}/>
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input 
+              type ="text" 
+              className='' 
+              placeholder="Password"
+              type="text"
+              name="password"
+              noValidate
+              onChange={this.handleChange}/>
+            </div>
+            <div className="createAccount">
+              <button type="submit">Create Account</button>
+              <small>Already Have an Account?</small>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default LoginForm;
+
+
+export default App;
--- cise2019/src/index.js
+++ cise2019/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-react';
 import './index.css';
 import App from './App';
+import Signup from './components/LoginForm'
 import * as serviceWorker from './serviceWorker';
 import Counter from './components/counter';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Signup />, document.getElementById('root'));
 serviceWorker.unregister();
