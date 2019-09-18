129d6eeeb331f14dfe40d790adad1f43be01839d
sherin-jacob <41213885+sherin-jacob@users.noreply.github.com>
2019-09-18 20:53:05 +1200

    trying to make page change on button click :(

--- cise2019/src/components/HomePage.js
+++ cise2019/src/components/HomePage.js
@@ -4,8 +4,15 @@ import LoginForm from './LoginForm.js';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PlantsImage from './images/PlantsImage.JPG';
 import ToolsImage from './images/ToolsImage.JPG';
+import Plants from './AboutPage.js';
+import Tools from './ToolsPage.js';
 
 export default class HomePage extends Component {
+  state = {
+    isPlantsVisible: false,
+    isToolsVisible: false,
+  }
+
   render() {
     return (
       <div className="container">
@@ -20,9 +27,11 @@ export default class HomePage extends Component {
           <Carousel></Carousel>  
           <br></br>
           <div className="plants-tools">
-            <img src={PlantsImage} className="plants-tools-images"/>
-            <img src={ToolsImage} className="plants-tools-images"/>
+            <img src={PlantsImage} className="plants-tools-images" onClick={() => this.setState({ isPlantsVisible: true, isToolsVisible: false }) }/>
+            <img src={ToolsImage} className="plants-tools-images" onClick={() => this.setState({ isToolsVisible: true, isPlantsVisible:false }) }/>
           </div>
+          { this.state.isPlantsVisible ? <Plants /> : null }
+          { this.state.isToolsVisible ? <Tools /> : null }
       </div>
     )
   }
