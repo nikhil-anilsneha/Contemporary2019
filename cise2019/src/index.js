a4f85c3f90bf9111638cc4ddf9a19f8ebc22a7ae
Nishant Patel <nishantpatel1842@gmail.com>
2019-09-19 12:15:29 +1200

    added missing lines of code in index.js

--- cise2019/src/index.js
+++ cise2019/src/index.js
@@ -1,6 +1,5 @@
-
-import Tree from './components/tree';
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'semantic-ui-react';
 import './index.css';
 import App from './App';
@@ -8,6 +7,7 @@ import Signup from './components/LoginForm'
 import * as serviceWorker from './serviceWorker';
 import Counter from './components/counter';
 import 'bootstrap/dist/css/bootstrap.css';
+import Tree from './components/tree';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
