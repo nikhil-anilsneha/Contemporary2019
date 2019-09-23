26ed3122cb266752b86c4a04d1b12d914e43d224
sherin-jacob <41213885+sherin-jacob@users.noreply.github.com>
2019-09-23 14:52:54 +1200

    still trying rip

--- cise2019/src/components/HomePage.js
+++ cise2019/src/components/HomePage.js
@@ -3,6 +3,7 @@ import Carousel from './Carousel';
 import PlantsImage from './images/PlantsImage.JPG';
 import ToolsImage from './images/ToolsImage.JPG';
 import ContactImage from './images/ContactImage.JPG';
+import {Link} from 'react-router-dom';
 
 export default class HomePage extends Component {
   render() {
--- cise2019/src/components/Tips.js
+++ cise2019/src/components/Tips.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 export default class Tips extends Component {
   render() {
     return (
-        <div>
-          <h3>Gardening 101</h3>
+        <div className="container">
+          <h1>Gardening 101</h1>
         </div>
     );
   }
