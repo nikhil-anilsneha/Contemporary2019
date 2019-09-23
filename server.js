2c8474db70b3069c064271fa349322d3ad380aef
sherin-jacob <41213885+sherin-jacob@users.noreply.github.com>
2019-09-24 10:07:27 +1200

    trying to figure database

--- server.js
+++ server.js
@@ -6,28 +6,47 @@ const app = express();
 
 const SELECT_ALL_PRODUCTS_QUERY = "SELECT * FROM trees";
 
-const connection = mysql.createConnection({
+/* const connection = mysql.createConnection({
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  user: "b72f7916dad1ba",
+  password: "bf43f260",
+  database: "heroku_25a635bc8a152af"
+}); */
+
+const pool = mysql.createPool({
   host: "us-cdbr-iron-east-02.cleardb.net",
   user: "b72f7916dad1ba",
   password: "bf43f260",
   database: "heroku_25a635bc8a152af"
 });
 
+
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("go to /products");
+  res.send("go to /products"); 
 });
 
-connection.connect(err => {
+/* connection.connect(err => {
   if (!err) console.log("DB connection succeded");
   else
     console.log(
       "DB Connection failed \n Error : " + JSON.stringify(err, undefined, 2)
     );
+}); */
+
+pool.getConnection(function(err, connection)
+{
+  connection.query(SELECT_ALL_PRODUCTS_QUERY, function(error, results, fields)
+  {
+    connection.release();
+    if (err) {
+      throw (error)
+    }
+  });
 });
 
-app.get("/products", (req, res) => {
+/* app.get("/products", (req, res) => {
   connection.query(SELECT_ALL_PRODUCTS_QUERY, (err, results) => {
     if (err) {
       return res.send(err);
@@ -37,7 +56,7 @@ app.get("/products", (req, res) => {
       });
     }
   });
-});
+}); */
 
 app.listen(3001, () => {
   console.log("products server listening on port 3001");
