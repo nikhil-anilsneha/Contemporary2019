3d7f31eedf81be9d44f61e921c7267d337c8b741
sherin-jacob <41213885+sherin-jacob@users.noreply.github.com>
2019-09-25 20:42:23 +1200

    working on tools page

--- cise2019/src/components/ToolsPage.js
+++ cise2019/src/components/ToolsPage.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link, withRouter } from 'react-router-dom';
 
+export default class ToolsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tools: []
+    };
+  }
 
-export default class AboutPage extends Component {
+  componentDidMount() {
+    this.getTools();
+  }
+
+  getTips = _ => {
+    fetch("http://localhost:3001/tools")
+      .then(response => response.json())
+      .then(response => this.setState({ tips: response.data[0] }))
+      .catch(err => console.error(err));
+  };
+
+  renderProduct = ({ tools_id, tools_name, tools_description }) => (
+    <div key={tools_id}>
+      <Link to={`/tools/${tools_id}`}>{tools_name}</Link>
+    </div>
+  );
+
   render() {
-    return (
-        <div>
-          <h3>Tools</h3>
-        </div>
-    );
+    const { tools } = this.state;
+    return <div>{tools.map(this.renderProduct) }</div>;
   }
 }
