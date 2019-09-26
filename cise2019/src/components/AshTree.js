ad3ca55bbbd5880b9d99ffa9bbb48f59e42f2bae
Nishant Patel <nishantpatel1842@gmail.com>
2019-09-26 14:32:09 +1200

    Updated renderProduct in Ash Tree, Updated Tree CSS & Updated package.json to run server and local
    Updated renderProduct in Ash Tree (AshTree.js), Updated Tree CSS (tree.css) & Updated package.json (package.json) to run server and local

--- cise2019/src/components/AshTree.js
+++ cise2019/src/components/AshTree.js
@@ -1,4 +1,13 @@
 import React, { Component } from "react";
+import './tree.css'
+import AshTreePic from "./images/AshTreePic.jpg";
+import OakTreePic from "./images/OakTreePic.jpg";
+import AppleTreePic from "./images/AppleTreePic.jpg";
+import TomatoTreePic from "./images/TomatoTreePic.jpg";
+import CascadePalmTreePic from "./images/CascadePalmTreePic.jpg";
+import KingPalmTreePic from "./images/KingPalmTreePic.jpg";
+import KauriTreePic from "./images/KauriTreePic.jpg";
+import PohutukawaTreePic from "./images/PohutukawaTreePic.jpg";
 
 export default class AshTree extends Component {
   constructor(props) {
@@ -19,10 +28,102 @@ export default class AshTree extends Component {
       .catch(err => console.error(err));
   };
 
-//   edit this part
+  getImage(tree_id) {
+    if(tree_id === 1)
+    {
+      return (
+        <div class = "tree-image">
+          <img src={AshTreePic} alt="AshTreePic" className="AshTreePic"></img>
+        </div>
+      );
+    }
+    else if(tree_id === 2)
+    {
+      return (
+        <div class = "tree-image">
+          <img src={OakTreePic} alt="OakTreePic" className="OakTreePic"></img>
+        </div>
+      );
+    }
+    else if(tree_id === 3)
+    {
+      return (
+        <div class = "tree-image">
+          <img src={AppleTreePic} alt="AppleTreePic" className="AppleTreePic"></img>
+        </div>
+      );
+    }
+    else if(tree_id === 4)
+    {
+      return (
+        <div class = "tree-image">
+          <img src={TomatoTreePic} alt="TomatoTreePic" className="TomatoTreePic"></img>
+        </div>
+      );
+    }
+    else if(tree_id === 5)
+    {
+      return (
+        <div class = "tree-image">
+          <img src={CascadePalmTreePic} alt="CascadePalmTreePic" className="CascadePalmTreePic"></img>
+        </div>
+      );
+    }
+    else if(tree_id === 6)
+    {
+      return (
+        <div class = "tree-image">
+          <img src={KingPalmTreePic} alt="KingPalmTreePic" className="KingPalmTreePic"></img>
+        </div>
+      );
+    }
+    else if(tree_id === 7)
+    {
+      return (
+        <div class = "tree-image">
+          <img src={KauriTreePic} alt="KauriTreePic" className="KauriTreePic"></img>
+        </div>
+      );
+    }
+    else if(tree_id === 8)
+    {
+      return (
+        <div class = "tree-image">
+          <img src={PohutukawaTreePic} alt="PohutukawaTreePic" className="PohutukawaTreePic"></img>
+        </div>
+      );
+    }
+    return tree_id
+  }
+
   renderProduct = ({ tree_id , tree_name, tree_type, tree_description }) => (
     <div key={tree_id}>
-      {tree_id} : {tree_name} <br/> {tree_type} <br/> {tree_description}<br/>
+
+      <div class = "tree-name">
+        <h1>{tree_name} Tree</h1>
+      </div>
+
+      <script>getImage({tree_id})</script>
+
+      <div class = "category">
+        <div class = "category-name">
+          <p>Tree Category</p>
+        </div>
+
+        <div class = "category-content">
+          <p>{tree_type}</p>
+        </div>        
+      </div>
+
+      <div class = "description">
+        <div class = "description-name">
+          <p>Tree Description</p>
+        </div>
+
+        <div class = "description-content">
+          <p>{tree_description}</p>
+        </div>        
+      </div>
     </div>
   );
 
