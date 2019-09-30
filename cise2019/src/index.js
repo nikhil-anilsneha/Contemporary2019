d723d83706401f8ef96678152e148aeb64462b36
sherin-jacob <41213885+sherin-jacob@users.noreply.github.com>
2019-09-30 17:28:34 +1300

    reinstalled nodes after change in dependencies

--- cise2019/src/index.js
+++ cise2019/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-react';
 import './index.css';
 import App from './App';
 import cartReducer from './components/reducers/cartReducer';
 import {Provider} from 'react-redux';
 import { createStore } from 'redux';
-// import Counter from './components/counter';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const store = createStore(cartReducer);
