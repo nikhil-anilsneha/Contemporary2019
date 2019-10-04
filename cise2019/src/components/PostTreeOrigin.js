af0c840efa21d6bb8b8ae1bc5ad0d1c6627cb433
Nishant Patel <nishantpatel1842@gmail.com>
2019-10-05 01:28:45 +1300

    Created Tree Origin and Tree Type Info - Still need to display and link to pages

---
+++ cise2019/src/components/PostTreeOrigin.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './tree.css'
+
+export default class PostTreeOrigin extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          tree: []
+        };
+      }
+    
+      componentDidMount() {
+        this.getTrees();
+      }
+      
+      getTrees = _ => {
+        fetch("/tree/")
+          .then(response => response.json())
+          .then(response => this.setState({ tree: response.data[this.props.match.params.treeId] }))
+          .catch(err => console.error(err));
+      };
+    
+      renderProduct = ({ tree_id, tree_name, tree_origin }) => (
+        <div key={tree_id}>
+            <div class = "tree-name">
+                <h1>{tree_name} Tree</h1>
+            </div>
+  
+            <div class = "header">
+                <div class = "header-name">
+                    <p>Tree Origin</p>
+                </div>
+  
+                <div class = "header-content">
+                    <p>{tree_origin}</p>
+                </div>        
+            </div>
+        </div>
+      );
+    
+      render() {
+        const { tree } = this.state;
+        return <div>{tree.map(this.renderProduct)}</div>;
+      }
+}
