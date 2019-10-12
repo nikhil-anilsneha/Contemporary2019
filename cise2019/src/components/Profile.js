09b87af8777d3153af447984b11c87f9a7e242a8
Nishant Patel <nishantpatel1842@gmail.com>
2019-10-13 02:00:12 +1300

    CSS Modified for Nav Bar, Profile, Title for Browsing Page & Made Logo Banner Redirect to Homepage on Click

--- cise2019/src/components/Profile.js
+++ cise2019/src/components/Profile.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
+import './Profile.css';
 
 export default class Profile extends Component {
   render() {
     return (
       <div className="profileContainer">
         <h1>Profile</h1>
-        <form>
-          <div className="profileName">
+        <br></br>
+        <div className="profileName">
           <h3>First Name: {window.globalFirstName}</h3>
           <br></br>
           <h3>Last Name: {window.globalLastName}</h3>
           <br></br>
           <h3>Email: {window.globalEmail}</h3>
-          <br></br>
-          </div>
-        </form>
+        </div>
       </div>
     );
   }
