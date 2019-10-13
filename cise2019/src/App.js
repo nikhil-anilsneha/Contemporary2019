96cf3c0bc60ab45ac113f15f96965e959a2285ad
nik0184 <41213901+nik0184@users.noreply.github.com>
2019-10-13 21:42:18 +1300

    removes hompage link from app.js

--- cise2019/src/App.js
+++ cise2019/src/App.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react';
 import './App.css';
 import banner from "./components/images/banner.jpg";
 import NavBar from './components/NavBar';
+import HomePage from './components/HomePage.js';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
         <div className="maindiv">
-          <a href="/HomePage.js"><img src={banner} alt="banner" className="banner img-responsive"></img></a>
+          <img src={banner} alt="banner" className="banner img-responsive"></img>
           <NavBar></NavBar>
         </div>
     );
